e1e50bfa26c0c2e3108fec548fcf27eec87e97d1
EvaIlievska <ilievska_eva@yahoo.com>
2023-12-25 18:38:43 +0100

    zadaca dva

---
+++ src/n_2.java
@@ -0,0 +1,13 @@
+
+public class n_2 {
+
+	public static void main(String[] args) {
+		int x = 20;
+		int y = myFunc(x);
+		System.out.println(y);
+	}
+    public static int myFunc(int x) {
+    	return x/2;
+    }
+}
+
